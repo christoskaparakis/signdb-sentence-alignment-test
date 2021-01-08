91d3e084819a708c7bdc8e14bfc45f9325407b07
Christos <christoskaparakis95@gmail.com>
2021-01-08 15:13:16 +0100

    fixed bug

--- script.py
+++ script.py
@@ -39,10 +39,10 @@ for file in os.listdir('videos and timestamp seg'):
             start = timestamps[0]
             print(start)
             end = timestamps[1]
-            stream = ffmpeg.input('videos and timestamp seg/video{}.ts'.format(counter_files))
-            stream = ffmpeg.trim(stream, start=start, end=end)  # start_pts, end_pts to use timestamps
-            stream = ffmpeg.output(stream, get_path_out(counter_files, counter_segments))
-            ffmpeg.run(stream)
+            # stream = ffmpeg.input('videos and timestamp seg/video{}.ts'.format(counter_files))
+            # stream = ffmpeg.trim(stream, start=start, end=end)  # start_pts, end_pts to use timestamps
+            # stream = ffmpeg.output(stream, get_path_out(counter_files, counter_segments))
+            # ffmpeg.run(stream)
 
 
             ###
@@ -53,31 +53,58 @@ for file in os.listdir('videos and timestamp seg'):
 
             i = 0
             for l in subs:
-                # print(l.start)  # shows the time in miliseconds
+                # print(l.start)  # shows the start timestamp in miliseconds
                 # print(l.end)
                 if i > 1 and l.start < endms:  # first 2 subs are the same in every file (date etc)
                     if l.start < startms < l.end:
                         # if subtitle starts before the video segment starts and ends while the video segment still
                         # plays
-                        if '.' in l.text:
-                            alignedsub += ' ' + subs[i].text.split('.')[1]
-                        elif '.' in subs[i - 1].text:
-                            alignedsub += ' ' + subs[i - 1].text.split('.')[1]
+                        if ('.' in l.text) or ('?' in l.text):
+                            if '.' in l.text:
+                                if subs[i].text.split('.')[1] != '':
+                                    alignedsub += ' ' + subs[i].text.split('.')[1]
+                                else:
+                                    alignedsub += ' ' + subs[i].text
+                            elif '?' in l.text:
+                                if subs[i].text.split('?')[1] != '':
+                                    alignedsub += ' ' + subs[i].text.split('?')[1]
+                                else:
+                                    alignedsub += ' ' + subs[i].text
+
+                        elif ('.' in subs[i - 1].text) or ('?' in subs[i - 1].text):
+                            if '.' in subs[i - 1].text:
+                                if subs[i-1].text.split('.')[1] != '':
+                                    alignedsub += ' ' + subs[i - 1].text.split('.')[1]
+                                else:
+                                    alignedsub += ' ' + subs[i].text
+                            elif '?' in subs[i - 1].text:
+                                if subs[i - 1].text.split('?')[1] != '':
+                                    alignedsub += ' ' + subs[i - 1].text.split('?')[1]
+                                else:
+                                    alignedsub += ' ' + subs[i].text
+
+                        else:
+                            alignedsub += ' ' + subs[i].text
+
                     elif l.start > startms and l.end < endms:
                         # if subtitle starts and ends while the video segment is playing
                         alignedsub += ' ' + subs[i].text
+
                     elif l.start < endms < l.end:
-                        # if subtitle starts before the video segment stops but ends after it stopped playing
-                        if '.' in l.text:
+                        # if subtitle starts before the video segment ends and continues after that
+                        if ('.' in l.text) or ('?' in l.text):
+                            if '.' in l.text:
+                                alignedsub += ' ' + subs[i].text.split('.')[1]
+                            elif '?' in l.text:
+                                alignedsub += ' ' + subs[i].text.split('?')[1]
+                        elif ('.' in subs[i - 1].text) or ('?' in subs[i - 1].text):
                             if '.' in subs[i - 1].text:
                                 alignedsub = alignedsub.replace(alignedsub.split('.')[-1], '')
-                            else:
-                                alignedsub += ' ' + subs[i].text.split('.')[0] + '.'
-                        else:
-                            alignedsub = alignedsub.replace(alignedsub.split('.')[-1], '')
+                            elif '?' in subs[i - 1].text:
+                                alignedsub = alignedsub.replace(alignedsub.split('?')[-1], '')
                 i += 1
-            print(alignedsub)
             alignedsub = alignedsub.replace("\\N", " ")
+            print(alignedsub)
             text_file = open('subtitle segments/subsegment{}.txt'.format(counter_segments), "w")
             text_file.write(alignedsub)
             text_file.close()
